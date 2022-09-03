41ef1553a971a13c21d7e15245ab95bd88545fc4
Nusrat Jahan <nusrat17175@gmail.com>
2022-09-03 20:51:18 +0600

    second commit

---
+++ explore.js
@@ -0,0 +1,10 @@
+const loadNewsTitle = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data.news_category);
+}
+
+
+loadNewsTitle();
+
--- index.js
+++ index.js
@@ -23,18 +23,18 @@ const displayPhones = data => {
               <h5 class="card-title">${data.title}</h5>
               <p class="card-text">${data.details.slice(0, 90)}</p>
             </div>
-            <div class="d-flex justify-content-between">
-            <div class="mx-3 d-flex">
+            <div class="d-flex justify-content-">
+            <div class="mx-3 d-flex justify-content-between">
               <img class="author-img rounded-circle mt-1" src="${data.author.img}">
               <div class="mx-3">
                 <h6>${data.author.name}</h6>
                 <p>${data.author.published_date}</p>
               </div>
-               <button class=""><i class="text-right fa-regular fa-eye"> ${data.total_view}</i></button>
               </div>
+              <a class="ms-auto m-4 text-black"> <i class="text-right fa-regular fa-eye"> ${data.total_view}</i></a>
             </div>
             <div class="card-footer text-center">
-             <button onclick="loadPhoneDetails('${data.author}')" class="btn text-muted" data-bs-toggle="modal" data-bs-target="#exampleModal">Show-details</button>
+             <button onclick="loadAuthorDetail('${data.author}')" class="btn text-muted" data-bs-toggle="modal" data-bs-target="#exampleModal">Show-details</button>
             </div>
           </div>
         </div>
@@ -44,21 +44,23 @@ const displayPhones = data => {
 }
 
 
-const loadPhoneDetails = async id => {
-  const url = `https://openapi.programming-hero.com/api/news/${id}`
-    const res = await fetch(url);
-    const data = await res.json();
-    displayPhoneDetails(data.data);
-}
-const displayPhoneDetails = data => {
-  console.log(data);
-  const modalTitle = document.getElementById('exampleModalLabel');
-  modalTitle.innerText = data.category_id;
-  const phoneDetails = document.getElementById('phone-details');
-  phoneDetails.innerHTML = `
-   
-  
-  `
-}
+const loadAuthorDetail = (news_id) => {
+  console.log(news_id);
+  fetch(`https://openapi.programming-hero.com/api/news/${news_id}`)
+    .then((res) => res.json())
+    .then((data) => displayAuthorDetail(data));
+};
 
+const displayAuthorDetail = (author) => {
+  const {name,published_date} = author;
+  const detailContainer = document.getElementById("author-details");
+  detailContainer.innerHTML = `
+     <div>
+        <h5 class="card-title">Author Name: ${name} </h5>
+        <p class="card-text">Publis date:${published_date}</p>
+        <p class="card-text">Author Bio:</p>
+     </div>
+     
+     `;
+}
 loadNews();
